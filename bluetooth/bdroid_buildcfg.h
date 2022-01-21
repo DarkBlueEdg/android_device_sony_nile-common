8bd75f3d270e55d66addafbbf3e305340daa6bfd
DarkBlueEdg <DarkBlueEdg@Gmail.Com>
2022-01-21 06:57:47 +0200

    small update part nr. 2/2
    small update part nr. 2/2

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -44,7 +44,7 @@ static inline const char* BtmGetDefaultName()
         return "Sony Xperia 10 Plus";
 
     // Fallback to ro.product.model
-    return "Xperia 10";
+    return "";
 }
 
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
